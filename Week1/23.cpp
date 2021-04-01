8c0f07a701d043b162c436eb06e8f16e88c32e85
HAJ931 <63355662+HAJ931@users.noreply.github.com>
2021-04-01 15:42:06 +0400

    Create 23.cpp
    Palindrome number

---
+++ Week1/23.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int temp, currDigit, reverse = 0;
+        if (x < 0)
+            return false;
+        temp = x;
+        while (temp != 0){
+            currDigit = temp % 10;
+            if (reverse > INT_MAX / 10 || (reverse == INT_MAX / 10 && currDigit > 7)) return 0;
+            if (reverse < INT_MIN / 10 || (reverse == INT_MIN / 10 && currDigit < -8)) return 0;
+            reverse = reverse * 10 + currDigit;
+            temp /= 10;
+        }
+        if(x == reverse)
+            return true;
+        return false;
+    }
+};
