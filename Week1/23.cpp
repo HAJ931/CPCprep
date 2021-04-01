862d08629f7f89605f36fa403dea9ae0f78ffd56
HAJ931 <63355662+HAJ931@users.noreply.github.com>
2021-04-01 16:09:00 +0400

    Update 23.cpp

--- Week1/23.cpp
+++ Week1/23.cpp
@@ -7,8 +7,10 @@ public:
         temp = x;
         while (temp != 0){
             currDigit = temp % 10;
-            if (reverse > INT_MAX / 10 || (reverse == INT_MAX / 10 && currDigit > 7)) return 0;
-            if (reverse < INT_MIN / 10 || (reverse == INT_MIN / 10 && currDigit < -8)) return 0;
+            if (reverse > INT_MAX / 10 || (reverse == INT_MAX / 10 && currDigit > 7)) 
+                return 0;
+            if (reverse < INT_MIN / 10 || (reverse == INT_MIN / 10 && currDigit < -8)) 
+                return 0;
             reverse = reverse * 10 + currDigit;
             temp /= 10;
         }
