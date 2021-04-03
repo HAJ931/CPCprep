9732f5591f8082e3447e4eb74a1872466e75369d
HAJ931 <63355662+HAJ931@users.noreply.github.com>
2021-04-04 00:59:59 +0400

    Create 2.cpp
    Find arr[x]

---
+++ Week2/2.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function to check if array has 2 elements
+	// whose sum is equal to the given value
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    // code here
+	    unordered_set<int> sumSet;
+	    int diff;
+	    for(int i = 0; i < n; ++i){
+	        diff = x - arr[i];
+        //Search if the difference exists in the set, if not, insert the current element in the set and continue
+	        if(sumSet.find(diff) != sumSet.end())
+	            return true;
+	       sumSet.insert(arr[i]);
+	    }
+	    return false;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.hasArrayTwoCandidates(arr, n, x);
+        cout << (ans ? "Yes\n" : "No\n");
+    }
+    return 0;
+}
+  // } Driver Code Ends
