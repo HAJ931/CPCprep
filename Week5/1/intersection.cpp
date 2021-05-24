3064c0cd1dda5a0c1e2e5ee7e6f5ec9e3780cef6
HAJ931 <63355662+HAJ931@users.noreply.github.com>
2021-05-24 19:24:14 +0400

    Create intersection.cpp
    Find intersection of two unordered arrays

---
+++ Week5/1/intersection.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int ptr1 = 0, ptr2 = 0, ptr = -1;
+        vector<int> inter;
+        while(ptr1 < nums1.size() && ptr2 < nums2.size()){
+            if(nums1[ptr1] == nums2[ptr2]){
+                if(ptr != -1 && inter[ptr] == nums1[ptr1])
+                    ++ptr1, ++ptr2;
+                else{
+                    ++ptr;
+                    inter.push_back(nums1[ptr1]);
+                    ++ptr1, ++ptr2;
+                }
+            }
+            else if(nums1[ptr1] < nums2[ptr2])
+                ++ptr1;
+            else
+                ++ptr2;
+        }
+        return inter;
+    }
+};
