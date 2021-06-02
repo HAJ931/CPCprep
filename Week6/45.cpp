83ad030deeb07a95671677d8e2904759736bdf5c
HAJ931 <63355662+HAJ931@users.noreply.github.com>
2021-06-03 01:56:55 +0400

    Create 45.cpp
    Remove nodes that don't lie within a certain range

---
+++ Week6/45.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int x){
+        this->data = x;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* removeRange(Node* root, int low, int high){
+    if(!root)
+        return NULL;
+    Node *left = removeRange(root->left, low, high);
+    Node *right = removeRange(root->right, low, high);
+    
+    if(root->data < low){
+        Node* rST = root->right;
+        delete root;
+        return rST;
+    }
+    else if(root->data > high){
+        Node *lST = root->left;
+        delete root;
+        return lST;
+    }
+    else
+        return root;
+    
+}
+
+void inOrder(Node* root){
+    if(!root)
+        return;
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+
+int main(){
+    Node* n1 = new Node(6);
+    n1->left = new Node(-13);
+    n1->right = new Node(14);
+    n1->left->right = new Node(-8);
+    n1->right->left = new Node(13);
+    n1->right->right = new Node(15);
+    n1->right->left->left = new Node(7);
+    n1 = removeRange(n1, -10, 13);
+    inOrder(n1);
+}
