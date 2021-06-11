acc30dbc6346a75e81f94bde354d1a82663044a7
HAJ931 <63355662+HAJ931@users.noreply.github.com>
2021-06-12 00:56:55 +0400

    Create 30.cpp
    Topological sort, Kahn's algorithm (Indegree Based Solution)

---
+++ Week7&8/30.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+
+class Solution
+{
+    private:
+    void DFS(int i, vector<int> adj[], vector<bool>& vis, stack<int>& s){
+        vis[i] = true;
+        for(auto x: adj[i]){
+            if(!vis[x]){
+                DFS(x, adj, vis, s);
+            }
+        }
+        s.push(i);
+    }
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    /*
+	    stack<int> s;
+	    vector<bool> vis(V, false);
+	    vector<int> ans;
+	    for(int i = 0; i < V; ++i){
+	        if(!vis[i])
+	            DFS(i, adj, vis, s);
+	    }
+	    while(!s.empty()){
+	        ans.push_back(s.top());
+	        s.pop();
+	    }
+	    return ans;
+	    */
+	    vector<int> inDeg(V, 0);
+	    for(int i = 0; i < V; ++i){
+	        for(auto x: adj[i]){
+	            ++inDeg[x];
+	        }
+	    }
+	    queue<int> q;
+	    int count = 0;
+	    for(int i = 0; i < inDeg.size(); ++i){
+	        if(inDeg[i] == 0){
+	            q.push(i);
+	            ++count;
+	        }
+	    }
+	    vector<int> res;
+	    while(!q.empty()){
+	        int currNode = q.front();
+	        q.pop();
+	        for(auto x: adj[currNode]){
+	            if(--inDeg[x] == 0){
+	                q.push(x);
+	                ++count;
+	            }
+	        }
+	        res.push_back(currNode);
+	    }
+	    if(count != V)
+	        return {};
+        return res;
+	}
+};
+
+// { Driver Code Starts.
+
+/*  Function to check if elements returned by user
+*   contains the elements in topological sorted form
+*   V: number of vertices
+*   *res: array containing elements in topological sorted form
+*   adj[]: graph input
+*/
+int check(int V, vector <int> &res, vector<int> adj[]) {
+    vector<int> map(V, -1);
+    for (int i = 0; i < V; i++) {
+        map[res[i]] = i;
+    }
+    for (int i = 0; i < V; i++) {
+        for (int v : adj[i]) {
+            if (map[i] > map[v]) return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, E;
+        cin >> E >> N;
+        int u, v;
+
+        vector<int> adj[N];
+
+        for (int i = 0; i < E; i++) {
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+        
+        Solution obj;
+        vector <int> res = obj.topoSort(N, adj);
+
+        cout << check(N, res, adj) << endl;
+    }
+    
+    return 0;
+}  // } Driver Code Ends
